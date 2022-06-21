4f4ca0ec241b136dca16abe3ce1b58c8ddfd6316
alphaxadLaravel <alphaxadfundi@gmail.com>
2022-06-21 11:27:02 -0700

    final

--- resources/views/common/welcome.blade.php
+++ resources/views/common/welcome.blade.php
@@ -235,7 +235,7 @@
                 <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
                     <div class="member">
                         <div class="member-img">
-                            <img src="{{ asset('img/team-22.jpg') }}"  class="img-fluid" >
+                            <img src="{{ asset('img/team-23.jpg') }}"  class="img-fluid" >
                         </div>
                         <div class="member-info">
                             <h4>David Bugingo</h4>
